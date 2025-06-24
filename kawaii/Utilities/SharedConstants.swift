6eb5aaddd278f2fe7776ec4eb90a0944ba02cd76
Los Mayers <carlostmayers@gmail.com>
2025-06-24 17:25:47 -0400

    add: new cycled texts

--- kawaii/Utilities/SharedConstants.swift
+++ kawaii/Utilities/SharedConstants.swift
@@ -8,16 +8,21 @@
 import Foundation
 
 struct AppConstants {
-    static let cyclingTexts = [
-        "this is not an app",
-        "this is your life",
-        "this was last month", 
-        "this is a video game",
-        "this was your life",
-        "this is people you miss",
-        "this is a faint memory",
-        "this feels like today",
-        "this is an app",
-        "this is your life with no fear"
-    ]
+	static let cyclingTexts = [
+		"this is not an app",
+		"this is your life",
+		"this was last month",
+		"this is a video game",
+		"this was your life",
+		"this is people you miss",
+		"this is a faint memory",
+		"this feels like today",
+		"this is an app",
+		"this is your life with no fear",
+		"this is not a memory",
+		"this is right now",
+		"this is your solitude",
+		"this is the start of your life",
+		"this is you happy",
+	]
 }
